a2d5db97464a2a11dbc0cba28362ba7560965285
rajasankar-sr <rajasankar.sr@taffinc.com>
2026-03-23 13:53:26 +0530

    changes the Icons assest inot direct SVG use

--- src/Pages/UserDetail.jsx
+++ src/Pages/UserDetail.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faCalendar, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+
 
 const API_URL = "https://69a720a32cd1d055268ff452.mockapi.io/tm_project";
 
@@ -171,34 +170,60 @@ const UserDetail = () => {
       </div>
 
       {/* USER INFO */}
-
-<div className="bg-white rounded-xl shadow mt-6 p-6">
+  {/* KEEP EVERYTHING INSIDE THIS BOX */}
+  <div className="bg-white rounded-xl shadow mt-6 p-6">
   <h3 className="font-semibold mb-4">User Information</h3>
 
-  {/* KEEP EVERYTHING INSIDE THIS BOX */}
-  <div className="grid grid-cols-3 gap-6">
+  <div className="grid grid-cols-3 gap-8 items-center">
 
     {/* Email */}
-    <div className="flex items-center gap-3">
-      <FontAwesomeIcon icon={faEnvelope} className="text-gray-400 text-lg" />
+    <div className="flex items-center gap-4">
+      <div className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-100 text-blue-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 640 640"
+          className="w-5 h-5"
+          fill="currentColor"
+        >
+          <path d="M64 160C64 124.7 92.7 96 128 96L512 96C547.3 96 576 124.7 576 160L576 480C576 515.3 547.3 544 512 544L128 544C92.7 544 64 515.3 64 480L64 160zM128 144C119.2 144 112 151.2 112 160L112 199.1L300.8 335.2C312.1 343.3 327.9 343.3 339.2 335.2L528 199.1L528 160C528 151.2 520.8 144 512 144L128 144zM528 260.9L366.4 375.6C338.2 395.5 301.8 395.5 273.6 375.6L112 260.9L112 480C112 488.8 119.2 496 128 496L512 496C520.8 496 528 488.8 528 480L528 260.9z"/>
+        </svg>
+      </div>
       <div>
         <p className="text-gray-500 text-sm">Email</p>
-        <p>{user.email}</p>
+        <p className="font-medium">{user.email}</p>
       </div>
     </div>
 
     {/* Member Since */}
-    <div className="flex items-center gap-3">
-      <FontAwesomeIcon icon={faCalendar} className="text-gray-400 text-lg" />
+    <div className="flex items-center gap-4">
+      <div className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-100 text-blue-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 640 640"
+          className="w-5 h-5"
+          fill="currentColor"
+        >
+          <path d="M216 64C229.3 64 240 74.7 240 88L240 128L400 128L400 88C400 74.7 410.7 64 424 64C437.3 64 448 74.7 448 88L448 128L480 128C515.3 128 544 156.7 544 192L544 480C544 515.3 515.3 544 480 544L160 544C124.7 544 96 515.3 96 480L96 192C96 156.7 124.7 128 160 128L192 128L192 88C192 74.7 202.7 64 216 64zM216 176L160 176C151.2 176 144 183.2 144 192L144 240L496 240L496 192C496 183.2 488.8 176 480 176L216 176zM144 288L144 480C144 488.8 151.2 496 160 496L480 496C488.8 496 496 488.8 496 480L496 288L144 288z"/>
+        </svg>
+      </div>
       <div>
         <p className="text-gray-500 text-sm">Member Since</p>
-        <p>25 February 2026</p>
+        <p className="font-medium">25 February 2026</p>
       </div>
     </div>
 
     {/* Status */}
-    <div className="flex items-center gap-3">
-      <FontAwesomeIcon icon={faCircleCheck} className="text-green-500 text-lg" />
+    <div className="flex items-center gap-4">
+      <div className="w-12 h-12 flex items-center justify-center rounded-full bg-green-100 text-green-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 640 640"
+          className="w-5 h-5"
+          fill="currentColor"
+        >
+          <path d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM320 112C205.1 112 112 205.1 112 320C112 434.9 205.1 528 320 528C434.9 528 528 434.9 528 320C528 205.1 434.9 112 320 112zM390.7 233.9C398.5 223.2 413.5 220.8 424.2 228.6C434.9 236.4 437.3 251.4 429.5 262.1L307.4 430.1C303.3 435.8 296.9 439.4 289.9 439.9C282.9 440.4 276 437.9 271.1 433L215.2 377.1C205.8 367.7 205.8 352.5 215.2 343.2C224.6 333.9 239.8 333.8 249.1 343.2L285.1 379.2L390.7 234z"/>
+        </svg>
+      </div>
       <div>
         <p className="text-gray-500 text-sm">Account Status</p>
         <p className="text-green-600 font-medium">Verified</p>
@@ -207,6 +232,7 @@ const UserDetail = () => {
 
   </div>
 </div>
+
 
       {/* ACTIVITY LOG */}
 
--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -4,8 +4,7 @@ import RegisterModal from "../Components/RegisterModal";
 import { UserContext } from "../Context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faUsers, faClipboard, faCircleCheck,faStar  } from "@fortawesome/free-solid-svg-icons";
+
 
 
 
@@ -191,8 +190,15 @@ const [showRegister, setShowRegister] = useState(false);
           <div className="grid md:grid-cols-3 gap-8 items-center perspective-[1000px]">
             {/* Administrator */}
             <div className="bg-white border border-red-200 rounded-xl shadow-sm p-11 text-center transform scale-95 transition-all duration-300  hover:scale-100 hover:-translate-y-3 hover:shadow-2xl">
-             <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-red-100 text-red-500 text-xl">
-  <FontAwesomeIcon icon={faStar} />
+          <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-red-100 text-red-500">
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 640 640"
+    className="w-5 h-5"
+    fill="currentColor"
+  >
+    <path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z"/>
+  </svg>
 </div>
 
               <h3 className="mt-4 text-lg font-semibold text-gray-900">
@@ -212,8 +218,15 @@ const [showRegister, setShowRegister] = useState(false);
 
             {/* Project Manager */}
             <div className="bg-white border border-blue-200 rounded-xl shadow-lg p-8 text-center transform scale-105 transition-all duration-300 hover:-translate-y-3 hover:shadow-2xl">
-            <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-blue-100 text-blue-500">
-  <FontAwesomeIcon icon={faClipboard} className="text-lg" />
+          <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-blue-100 text-blue-500">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 640"
+    className="w-5 h-5"
+    fill="currentColor"
+  >
+    <path d="M360 160L280 160C266.7 160 256 149.3 256 136C256 122.7 266.7 112 280 112L360 112C373.3 112 384 122.7 384 136C384 149.3 373.3 160 360 160zM360 208C397.1 208 427.6 180 431.6 144L448 144C456.8 144 464 151.2 464 160L464 512C464 520.8 456.8 528 448 528L192 528C183.2 528 176 520.8 176 512L176 160C176 151.2 183.2 144 192 144L208.4 144C212.4 180 242.9 208 280 208L360 208zM419.9 96C407 76.7 385 64 360 64L280 64C255 64 233 76.7 220.1 96L192 96C156.7 96 128 124.7 128 160L128 512C128 547.3 156.7 576 192 576L448 576C483.3 576 512 547.3 512 512L512 160C512 124.7 483.3 96 448 96L419.9 96z"/>
+  </svg>
 </div>
 
               <h3 className="mt-4 text-lg font-semibold text-gray-900">
@@ -235,8 +248,15 @@ const [showRegister, setShowRegister] = useState(false);
 
             {/* Team Member */}
             <div className="bg-white border border-red-200 rounded-xl shadow-sm p-8 text-center transform scale-95 transition-all duration-300  hover:scale-100 hover:-translate-y-3 hover:shadow-2xl">
-              <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-green-100 text-green-500">
-  <FontAwesomeIcon icon={faCircleCheck} className="text-lg" />
+             <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-green-100 text-green-500">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 640"
+    className="w-5 h-5"
+    fill="currentColor"
+  >
+    <path d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM320 112C205.1 112 112 205.1 112 320C112 434.9 205.1 528 320 528C434.9 528 528 434.9 528 320C528 205.1 434.9 112 320 112zM390.7 233.9C398.5 223.2 413.5 220.8 424.2 228.6C434.9 236.4 437.3 251.4 429.5 262.1L307.4 430.1C303.3 435.8 296.9 439.4 289.9 439.9C282.9 440.4 276 437.9 271.1 433L215.2 377.1C205.8 367.7 205.8 352.5 215.2 343.2C224.6 333.9 239.8 333.8 249.1 343.2L285.1 379.2L390.7 234z"/>
+  </svg>
 </div>
 
               <h3 className="mt-4 text-lg font-semibold text-gray-900">
@@ -280,9 +300,16 @@ const [showRegister, setShowRegister] = useState(false);
 
   {/* Step 1 */}
   <div className="flex flex-col items-center">
-    <div className="w-14 h-14 flex items-center justify-center rounded-full bg-white border border-blue-300 text-blue-600 text-xl">
-      <FontAwesomeIcon icon={faPlus} />
-    </div>
+    <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-white border border-blue-200 text-blue-500 shadow-sm">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 640"
+    className="w-5 h-5"
+    fill="currentColor"
+  >
+    <path d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/>
+  </svg>
+</div>
 
     <div className="mt-4 px-16 py-2 text-xs rounded-full font-bold bg-blue-200 text-blue-600">
       Step 1
@@ -294,9 +321,16 @@ const [showRegister, setShowRegister] = useState(false);
 
   {/* Step 2 */}
   <div className="flex flex-col items-center">
-    <div className="w-14 h-14 flex items-center justify-center rounded-full bg-white border border-blue-300 text-blue-600 text-xl">
-      <FontAwesomeIcon icon={faUsers} />
-    </div>
+   <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-white text-blue-600">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 640"
+    className="w-5 h-5"
+    fill="currentColor"
+  >
+    <path d="M320 80C377.4 80 424 126.6 424 184C424 241.4 377.4 288 320 288C262.6 288 216 241.4 216 184C216 126.6 262.6 80 320 80zM96 152C135.8 152 168 184.2 168 224C168 263.8 135.8 296 96 296C56.2 296 24 263.8 24 224C24 184.2 56.2 152 96 152zM0 480C0 409.3 57.3 352 128 352C140.8 352 153.2 353.9 164.9 357.4C132 394.2 112 442.8 112 496L112 512C112 523.4 114.4 534.2 118.7 544L32 544C14.3 544 0 529.7 0 512L0 480zM521.3 544C525.6 534.2 528 523.4 528 512L528 496C528 442.8 508 394.2 475.1 357.4C486.8 353.9 499.2 352 512 352C582.7 352 640 409.3 640 480L640 512C640 529.7 625.7 544 608 544L521.3 544zM472 224C472 184.2 504.2 152 544 152C583.8 152 616 184.2 616 224C616 263.8 583.8 296 544 296C504.2 296 472 263.8 472 224zM160 496C160 407.6 231.6 336 320 336C408.4 336 480 407.6 480 496L480 512C480 529.7 465.7 544 448 544L192 544C174.3 544 160 529.7 160 512L160 496z"/>
+  </svg>
+</div>
 
     <div className="mt-4 px-16 py-2 text-xs rounded-full font-bold bg-blue-200 text-blue-600">
       Step 2
@@ -308,9 +342,16 @@ const [showRegister, setShowRegister] = useState(false);
 
   {/* Step 3 */}
   <div className="flex flex-col items-center">
-    <div className="w-14 h-14 flex items-center justify-center rounded-full border bg-white border-blue-300 text-blue-600 text-xl">
-      <FontAwesomeIcon icon={faClipboard} />
-    </div>
+    <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-white text-blue-600">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 640"
+    className="w-5 h-5"
+    fill="currentColor"
+  >
+    <path d="M360 160L280 160C266.7 160 256 149.3 256 136C256 122.7 266.7 112 280 112L360 112C373.3 112 384 122.7 384 136C384 149.3 373.3 160 360 160zM360 208C397.1 208 427.6 180 431.6 144L448 144C456.8 144 464 151.2 464 160L464 512C464 520.8 456.8 528 448 528L192 528C183.2 528 176 520.8 176 512L176 160C176 151.2 183.2 144 192 144L208.4 144C212.4 180 242.9 208 280 208L360 208zM419.9 96C407 76.7 385 64 360 64L280 64C255 64 233 76.7 220.1 96L192 96C156.7 96 128 124.7 128 160L128 512C128 547.3 156.7 576 192 576L448 576C483.3 576 512 547.3 512 512L512 160C512 124.7 483.3 96 448 96L419.9 96z"/>
+  </svg>
+</div>
 
     <div className="mt-4 px-16 py-2 text-xs rounded-full font-bold bg-blue-200 text-blue-600">
       Step 3
@@ -322,9 +363,16 @@ const [showRegister, setShowRegister] = useState(false);
 
   {/* Step 4 */}
   <div className="flex flex-col items-center">
-    <div className="w-14 h-14 flex items-center justify-center rounded-full border bg-white border-blue-300 text-blue-600 text-xl">
-      <FontAwesomeIcon icon={faCircleCheck} />
-    </div>
+  <div className="w-12 h-12 mx-auto flex items-center justify-center rounded-full bg-white text-green-500">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 640"
+    className="w-5 h-5"
+    fill="currentColor"
+  >
+    <path d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM320 112C205.1 112 112 205.1 112 320C112 434.9 205.1 528 320 528C434.9 528 528 434.9 528 320C528 205.1 434.9 112 320 112zM390.7 233.9C398.5 223.2 413.5 220.8 424.2 228.6C434.9 236.4 437.3 251.4 429.5 262.1L307.4 430.1C303.3 435.8 296.9 439.4 289.9 439.9C282.9 440.4 276 437.9 271.1 433L215.2 377.1C205.8 367.7 205.8 352.5 215.2 343.2C224.6 333.9 239.8 333.8 249.1 343.2L285.1 379.2L390.7 234z"/>
+  </svg>
+</div>
 
     <div className="mt-4 px-16 py-2 text-xs rounded-full font-bold bg-blue-200 text-blue-600">
       Step 4
